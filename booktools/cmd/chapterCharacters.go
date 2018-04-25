86820c16b25ece077ca73c69361691833f577cd7
Howard C. Shaw III <howardcshaw@gmail.com>
2018-04-24 20:35:49 -0500

    Fixed license headers.

--- booktools/cmd/chapterCharacters.go
+++ booktools/cmd/chapterCharacters.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2018 Howard C. Shaw III <howardcshaw@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
